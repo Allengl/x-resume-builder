c5826189fe672f8d5dd09c7349725df47f2cb524
Allengl <guolong613@gmail.com>
2024-11-28 09:30:59 +0800

    add ResumeEditor.tsx

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
       {children}
     </div>
