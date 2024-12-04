e339d390b20fd0d2c3415ddf13d4a35ace75e9b0
Allengl <guolong613@gmail.com>
2024-12-04 15:17:25 +0800

    add ResumeItem.tsx

---
+++ src/app/(main)/resumes/ResumeItem.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import ResumePreview from "@/components/ResumePreview";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ResumeServerData } from "@/lib/type";
+import { mapToResumeValues } from "@/lib/utils";
+import { useState, useTransition } from "react";
+import { format } from "date-fns";
+import Link from "next/link";
+import { MoreVerticalIcon, Trash2 } from "lucide-react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogHeader,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { useToast } from "@/hooks/use-toast";
+import { deleteResume } from "./actions";
+import LoadingButton from "@/components/LoadingButton";
+
+interface ResumeItemProps {
+  resume: ResumeServerData;
+}
+
+export default function ResumeItem({ resume }: ResumeItemProps) {
+  const wasUpdated = resume.updatedAt !== resume.createdAt;
+
+  return (
+    <div className="group relative rounded-lg border border-transparent bg-secondary p-3 transition-colors hover:border-border">
+      <div className="space-y-3">
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="inline-block w-full text-center"
+        >
+          <p className="line-clamp-1 font-semibold">
+            {resume.title || "No title"}
+          </p>
+          <p>
+            {resume.description && (
+              <span className="line-clamp-2 text-sm">{resume.description}</span>
+            )}
+          </p>
+          <p className="text-xs text-muted-foreground">
+            {wasUpdated ? "Updated" : "Created"} on{" "}
+            {format(resume.updatedAt, "MMM d, yyyy h:mm a")}
+          </p>
+        </Link>
+        <Link href={`/editor?resumeId=${resume.id}`} className="w-full">
+          <ResumePreview
+            resumeData={mapToResumeValues(resume)}
+            className="overflow-hidden shadow-sm transition-shadow group-hover:shadow-lg"
+          />
+          <div className="absolute inset-x-0 bottom-0 h-16 bg-gradient-to-t from-white to-transparent" />
+        </Link>
+      </div>
+      <MoreMenu resumeId={resume.id} />
+    </div>
+  );
+}
+
+interface MoreMenuProps {
+  resumeId: string;
+}
+function MoreMenu({ resumeId }: MoreMenuProps) {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute right-0.5 top-0.5 opacity-0 transition-opacity group-hover:opacity-100"
+          >
+            <MoreVerticalIcon className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={() => setShowDeleteConfirmation(true)}
+          >
+            <Trash2 className="size-4" />
+            <span>Delete</span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteConfirmationDialog
+        resumeId={resumeId}
+        open={showDeleteConfirmation}
+        onOpenChange={setShowDeleteConfirmation}
+      />
+    </>
+  );
+}
+
+interface DeleteConfirmationDialogProps {
+  resumeId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+function DeleteConfirmationDialog({
+  resumeId,
+  open,
+  onOpenChange,
+}: DeleteConfirmationDialogProps) {
+  const { toast } = useToast();
+  const [isPending, startTransition] = useTransition();
+
+  async function handleDelete() {
+    startTransition(async () => {
+      try {
+        await deleteResume(resumeId);
+        onOpenChange(false);
+      } catch (error) {
+        console.error(error);
+        toast({
+          variant: "destructive",
+          description: "Something went wrong. Please try again.",
+        });
+      }
+    });
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete Resume?</DialogTitle>
+          <DialogDescription>
+            This will permanently delete this resume. This action cannot be
+            undone.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <LoadingButton
+            variant="destructive"
+            onClick={handleDelete}
+            loading={isPending}
+          >
+            Delete
+          </LoadingButton>
+          <Button variant="secondary" onClick={() => onOpenChange(false)}>
+            Cancel
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
